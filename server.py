73fecb17d984709f79a372d3952a285f93744e59
jadashipp <jada.l.shipp@gmail.com>
2020-06-16 11:44:17 -0500

    dice function working

--- server.py
+++ server.py
@@ -1,7 +1,14 @@
 from flask import Flask
+import numpy
+
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return 'Hello, World!'
+
+@app.route('/roll-dice')
+def dice(): 
+    roll = numpy.random.randint(1,6) 
+    return f"{roll}"
